b78e86292da0eef227037ffd6eda1858ad64f822
Sunny Kumar <sunny@techformation.io>
2025-04-04 17:15:40 +0530

    inherit model attributes from schema

--- database/models/User.ts
+++ database/models/User.ts
@@ -1,30 +1,12 @@
-import { Model } from "sequelize";
+import { Model, InferAttributes, InferCreationAttributes } from "sequelize";
 import sequelize from "@lib/db";
 
 const userSchema = require("../schemas/userSchema");
 
-class User extends Model {
-  declare id: number;
-  declare email: string;
-  declare password_hash: string;
-  declare passwordHash: string;
-  declare firstName: string;
-  declare lastName: string;
-  declare city: string;
-  declare state: string;
-  declare phone: string;
-  declare refreshToken: string;
-  declare secondaryPhone: string;
-  declare isActive: boolean;
-  declare status: string;
-  declare lastLogin: Date | null;
-  declare createdAt: Date;
-  declare updatedAt: Date;
-  declare deletedAt: Date | null;
-  declare address: string;
-  declare zip: string;
-  declare UserRole?: any;
-}
+class User extends Model<
+  InferAttributes<User>, // Infers attributes from schema
+  InferCreationAttributes<User> // For creation (optional fields)
+> {}
 
 User.init(userSchema, {
   sequelize,
